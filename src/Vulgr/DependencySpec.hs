a58d5b366c0d600ff2c5cb0e7d5f50af71247795
Stephen O'Brien <wayofthepie@gmail.com>
2016-03-11 23:14:04 +0000

    Enforces uniquenes in nodes

--- src/Vulgr/DependencySpec.hs
+++ src/Vulgr/DependencySpec.hs
@@ -14,7 +14,8 @@ import Control.Monad.Trans.Reader
 import Control.Monad.Trans.Resource (runResourceT, ResourceT)
 import Control.Retry
 import Data.Functor
-import qualified Data.HashMap.Strict as M
+import Data.Hashable
+import qualified Data.HashMap.Strict as H
 import Data.Monoid
 import qualified Data.Text as T
 --import qualified Database.Neo4j as Neo
@@ -42,6 +43,9 @@ data NodeData = NodeData
     , labels  :: [T.Text]
     } deriving (Eq, Show)
 
+instance Hashable NodeData where
+    hashWithSalt s (NodeData n v ls) = s + hash n + hash v + hash ls
+
 data RelationData = RelationData
     { config   :: T.Text
     , rootNode :: NodeData
@@ -51,15 +55,15 @@ class Graphable s where
     graph :: s -> Gr NodeData RelationData
 
 type DependencyGraph = Gr NodeData RelationData
-type GraphState = (Int, DependencyGraph)
+type GraphState = (H.HashMap NodeData Int, DependencyGraph)
 --type GraphState = State (Int, DependencyGraph) DependencyGraph
 
 
 instance Graphable GradleDependencySpec where
     graph gradleDeps =
        let rootNode  = NodeData (gDepName gradleDeps) (gDepVersion gradleDeps) []
-           initGraph = insNode (0,rootNode) empty
-       in  snd $ foldr (\config grs -> parseConfig grs config) (0,initGraph) $ gDepConfigs gradleDeps
+           initGraphState = (H.empty, insNode (0,rootNode) empty)
+       in  snd $ foldr (\config grs -> parseConfig grs config) initGraphState $ gDepConfigs gradleDeps
 
 
 parseConfig :: GraphState -> Configuration -> GraphState
@@ -71,17 +75,15 @@ parseConfig grs (Configuration configName _ (Just deps)) = do
 
 parseDependencies :: GraphState -> Int -> [Dependency] -> T.Text -> GraphState
 parseDependencies grs  _ [] _ = grs
-parseDependencies grs parentIx (dep:deps) configName =
-    let currIx    = fst grs
-        currGraph = snd grs
-        thisIx    = currIx + 1
-        thisNode  = NodeData (depName dep) Nothing []
-        g = consEdge parentIx thisIx configName $ consNode thisIx thisNode currGraph
+parseDependencies grs parentNodeId (dep:deps) configName =
+    let thisNode  = NodeData (depName dep) Nothing []
+        (thisNodeId, (m, g)) = consNode thisNode grs
+        g' = consEdge parentNodeId thisNodeId configName g
     in case depChildren dep of
         Just children ->  case children of
-                            [] -> parseDependencies (thisIx, g) thisIx deps configName
-                            _  -> parseDependencies (thisIx, g) thisIx children configName
-        Nothing ->  parseDependencies (thisIx, g) parentIx deps configName
+                            [] -> parseDependencies (m, g') thisNodeId deps configName
+                            _  -> parseDependencies (m, g') thisNodeId children configName
+        Nothing ->  parseDependencies (m, g') parentNodeId deps configName
   where
     getRootNode :: DependencyGraph -> NodeData
     getRootNode currGraph =
@@ -94,6 +96,15 @@ parseDependencies grs parentIx (dep:deps) configName =
     consEdge from to configName g =
         insEdge (from, to, RelationData configName (getRootNode g)) g
 
-    consNode :: Int -> NodeData -> DependencyGraph -> DependencyGraph
-    consNode ix ndat = insNode (ix, ndat)
+    -- | Construct a new node in the given dependency graph.
+    consNode :: NodeData -> GraphState -> (Int, GraphState)
+    consNode ndata (m, g) = create $ H.lookup ndata m
+      where
+        -- | Create a node in the graph if it does not already exist.
+        create :: Maybe Int -> (Int, GraphState)
+        create (Just nid) = (nid, (m, insNode (nid, ndata) g))
+        create Nothing    = traceShow ("New node being created for " <> show ndata) $
+            let [newNid] = newNodes 1 g
+                m'       = H.insert ndata newNid m
+            in  (newNid, (m', insNode (newNid, ndata) g))
 
